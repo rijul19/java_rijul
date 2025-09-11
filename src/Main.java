c127eba0977f2a567587b33ac6f34b70b4a6ba9c
Rijul <rijul0688.becse24@chitkara.edu.in>
2025-09-11 10:57:29 +0530

    hello

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+//public class Main{
+//    public static void main(String args[]){
+//        System.out.println("hellooooooo");
+//    }
+//}
+
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//    }
+//}
+
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Number of arguments: " + args.length);
+//        for (String arg : args) {
+//            System.out.println(arg);
+//        }
+//    }
+//}
+
+//import java.util.*;
+//public class Main {
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        System.out.println(a + b);
+//        sc.close();
+//    }
+//        }
+
+//import java.util.*;
+//public class Main{
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        System.out.println(a+b);
+//        System.out.println(a-b);
+//        System.out.println(a*b);
+//        System.out.println(a/b);
+//        System.out.println(a%b);
+//    }
+//}
+
+//public class Main {
+//    public static void main(String[] args) {
+//        for (int i = 1; i <= 30; i++) {
+//            if (i % 2 != 0) {
+//                System.out.println(i);
+//            }
+//        }
+//    }
+
+public class Main{
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            if (i > 5) {
+                System.out.println(i + " -> High");
+            } else {
+                System.out.println(i + " -> Low");
+            }
+            i++;
+        }
+    }
+}
+
+
